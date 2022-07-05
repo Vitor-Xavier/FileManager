33f7f005dd64d5e4b91c5782eb45ac13b55657c2
Vitor Xavier de Souza <vitorvxs@live.com>
2022-07-05 12:08:30 -0300

    fix: StorageFile model

---
+++ Models/StorageFile.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FileManager.Models
+{
+    public class StorageFile
+    {
+        public int StorageFileId { get; set; }
+
+        [StringLength(30)]
+        public string Owner { get; set; }
+
+        [StringLength(255)]
+        public string FileName { get; set; }
+
+        [StringLength(2000)]
+        public string Path { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public DateTime UpdatedAt { get; set; }
+
+        public bool Deleted { get; set; }
+    }
+}
